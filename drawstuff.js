9047ec4a7cc0ed4f7e94556484c9358e5a09f827
jtgill <jtgill@ncsu.edu>
2018-09-22 17:52:44 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -607,7 +607,7 @@ function main() {
     var canvas = document.getElementById("viewport"); 
     var context = canvas.getContext("2d");
     
-    rayCasting(context);
+    //rayCasting(context);
  
     // Create the image
     //drawRandPixels(context);
@@ -619,7 +619,7 @@ function main() {
     //drawInputEllipsoidsUsingArcs(context);
       // shows how to read input file, but not how to draw pixels
     
-    //drawRandPixelsInInputTriangles(context);
+    drawRandPixelsInInputTriangles(context);
     // shows how to draw pixels and read input file
     
     //drawInputTrainglesUsingPaths(context);
